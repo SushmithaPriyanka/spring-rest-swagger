78e30e6239ab6dbeb46799cfabcfeb8c1efcd1d1
SushmithaPriyanka <sushmitha7908@gmail.com>
2020-04-26 15:47:18 +0530

    initial version

---
+++ PrintMessage.java
@@ -0,0 +1,8 @@
+import java.util.io.*;
+import java.util.io.*;
+public class PrintMessage{
+    public static void main(String args[]){
+        String msg= "Welcome to object oriented programming";
+        System.out.println(msg);
+    }
+}
